c15b075fcda7ec40cb8b56dd9376a68e1072ede7
Nahid4306053 <131442604+Nahid4306053@users.noreply.github.com>
2023-06-20 01:15:49 +0600

    Textilar web template

---
+++ Readme.txt
@@ -0,0 +1,6 @@
+# TEXTILE COMPANY WEB TEMPLATE 
+
+Template Name: TEXTILAR
+Template URL: https://nahid4306053.github.io/textile/	
+Author: Md Nahid Hasan
+License: https://github.com/Nahid4306053
---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+
+  $(function () {
+    $(window).on("scroll", function () {
+      if ($(window).scrollTop() > 100) {
+        $("#navbars").addClass("fixed-top");
+      } else {
+        //remove the background property so it comes transparent again (defined in your css)
+        $("#navbars").removeClass("fixed-top");
+      }
+    });
+  });
+
+
+  $('#project').counterUp({
+    delay: 10,
+    time: 10000
+  });  
+  $('#project-1').counterUp({
+    delay: 10,
+    time: 1000
+  });
+  $('.counters').counterUp({
+    delay: 10,
+    time: 2000
+  });   $('.countersi').counterUp({
+    delay: 10,
+    time: 2000
+  });  
+
+
+
+  var owl = $('#banner-crasoul .owl-carousel');
+  owl.owlCarousel({
+      items:1,
+      loop:true,
+      margin:10,
+      autoplay:true,
+      autoplayTimeout:2000,
+      autoplayHoverPause:true,
+      animateOut: 'fadeOut'
+  }); 
+  
+  var owl = $('#client .owl-carousel');
+  owl.owlCarousel({
+      items:1,
+      loop:true,
+      margin:10,
+      autoplay:true,
+      autoplayTimeout:2000,
+      autoplayHoverPause:true,
+      
+  });  
+  var owl = $('.project-blog');
+  owl.owlCarousel({
+      items:2,
+      loop:true,
+      margin:10,
+      autoplay:true,
+      autoplayTimeout:2000,
+      autoplayHoverPause:true,
+      
+  });
+
+
+  AOS.init();
+
+});
